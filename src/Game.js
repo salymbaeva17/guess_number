5640b7baf69e53fdf2b51ee686ff0d0d64fbb8c9
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-08-23 09:21:38 +0600

    delete useEffect

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 
 const Game = () => {
     const [number, setNumber] = useState(Math.round(Math.random() * 10))
